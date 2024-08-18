2a85b443971543d31c4f16cc688f5fef59a98ba6
Dhananjay Sahoo <kanha94377@gmail.com>
2024-08-18 11:37:16 +0530

    update v0.0.3

--- src/app/page.js
+++ src/app/page.js
@@ -2,29 +2,80 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="bg-red-100">
-      <div className="  text-white-600 font-bold rounded py-2 mx-3 ">
-        <div>
-          <div className="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-lg flex items-center space-x-4">
-            <div className="shrink-0">
-              <Image
-                className="h-12 w-12"
-                src="/prof.png"
-                width={900}
-                height={500}
-                alt="ChitChat Logo"
-              />
-             
-            </div>
-            <div>
-              <div className="text-xl font-medium text-black">Dhananjay</div>
-              {/* <p className="text-slate-500">You have a new message!</p> */}
+    <div className="">
+     <div>
+     <div
+        class="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80"
+        aria-hidden="true">
+        <div class="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"></div>
+      </div>
+
+      <div>
+      <div className="">
+        <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+          <div className="mx-auto max-w-2xl py-16 sm:py-24 lg:max-w-none lg:py-32">
+            <h2 className="text-2xl font-bold text-gray-900">Collections</h2>
+
+            <div className="mt-6 space-y-12 lg:grid lg:grid-cols-3 lg:gap-x-6 lg:space-y-0">
+              <div className="group relative">
+                <div className="relative h-80 w-full overflow-hidden rounded-lg bg-white sm:aspect-h-1 sm:aspect-w-2 lg:aspect-h-1 lg:aspect-w-1 group-hover:opacity-75 sm:h-64">
+                  <img
+                    src="https://tailwindui.com/img/ecommerce-images/home-page-02-edition-01.jpg"
+                    alt="Desk with leather desk pad, walnut desk organizer, wireless keyboard and mouse, and porcelain mug."
+                    className="h-full w-full object-cover object-center"
+                  />
+                </div>
+                <h3 className="mt-6 text-sm text-gray-500">
+                  <a href="#">
+                    <span className="absolute inset-0"></span>
+                    Desk and Office
+                  </a>
+                </h3>
+                <p className="text-base font-semibold text-gray-900">
+                  Work from home accessories
+                </p>
+              </div>
+              <div className="group relative">
+                <div className="relative h-80 w-full overflow-hidden rounded-lg bg-white sm:aspect-h-1 sm:aspect-w-2 lg:aspect-h-1 lg:aspect-w-1 group-hover:opacity-75 sm:h-64">
+                  <img
+                    src="https://tailwindui.com/img/ecommerce-images/home-page-02-edition-02.jpg"
+                    alt="Wood table with porcelain mug, leather journal, brass pen, leather key ring, and a houseplant."
+                    className="h-full w-full object-cover object-center"
+                  />
+                </div>
+                <h3 className="mt-6 text-sm text-gray-500">
+                  <a href="#">
+                    <span className="absolute inset-0"></span>
+                    Self-Improvement
+                  </a>
+                </h3>
+                <p className="text-base font-semibold text-gray-900">
+                  Journals and note-taking
+                </p>
+              </div>
+              <div className="group relative">
+                <div className="relative h-80 w-full overflow-hidden rounded-lg bg-white sm:aspect-h-1 sm:aspect-w-2 lg:aspect-h-1 lg:aspect-w-1 group-hover:opacity-75 sm:h-64">
+                  <img
+                    src="https://tailwindui.com/img/ecommerce-images/home-page-02-edition-03.jpg"
+                    alt="Collection of four insulated travel bottles on wooden shelf."
+                    className="h-full w-full object-cover object-center"
+                  />
+                </div>
+                <h3 className="mt-6 text-sm text-gray-500">
+                  <a href="#">
+                    <span className="absolute inset-0"></span>
+                    Travel
+                  </a>
+                </h3>
+                <p className="text-base font-semibold text-gray-900">
+                  Daily commute essentials
+                </p>
+              </div>
             </div>
           </div>
         </div>
-      </div>
-
-      <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
+        
+      </div><div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
         <h2 className="text-2xl font-bold tracking-tight text-gray-900">
           Customers also purchased
         </h2>
@@ -42,7 +93,10 @@ export default function Home() {
               <div>
                 <h3 className="text-sm text-gray-700">
                   <a href="#">
-                    <span aria-hidden="true" className="absolute inset-0"></span>
+                    <span
+                      aria-hidden="true"
+                      className="absolute inset-0"
+                    ></span>
                     Basic Tee
                   </a>
                 </h3>
@@ -63,7 +117,10 @@ export default function Home() {
               <div>
                 <h3 className="text-sm text-gray-700">
                   <a href="#">
-                    <span aria-hidden="true" className="absolute inset-0"></span>
+                    <span
+                      aria-hidden="true"
+                      className="absolute inset-0"
+                    ></span>
                     Basic Tee
                   </a>
                 </h3>
@@ -84,7 +141,10 @@ export default function Home() {
               <div>
                 <h3 className="text-sm text-gray-700">
                   <a href="#">
-                    <span aria-hidden="true" className="absolute inset-0"></span>
+                    <span
+                      aria-hidden="true"
+                      className="absolute inset-0"
+                    ></span>
                     Basic Tee
                   </a>
                 </h3>
@@ -105,7 +165,10 @@ export default function Home() {
               <div>
                 <h3 className="text-sm text-gray-700">
                   <a href="#">
-                    <span aria-hidden="true" className="absolute inset-0"></span>
+                    <span
+                      aria-hidden="true"
+                      className="absolute inset-0"
+                    ></span>
                     Basic Tee
                   </a>
                 </h3>
@@ -116,6 +179,15 @@ export default function Home() {
           </div>
         </div>
       </div>
+      </div>
+     
+      <div
+          class="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]"
+          aria-hidden="true"
+        >
+          <div class="relative left-[calc(50%+3rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%+36rem)] sm:w-[72.1875rem]"></div>
+        </div>
+     </div>
     </div>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -3,13 +3,13 @@ import Image from "next/image";
 export default function Header() {
     return (
         <div className="bg-white">
-            <header className="bg-white">
+            <header className="">
   <nav className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8" aria-label="Global">
     <div className="flex lg:flex-1">
       <a href="#" className="-m-1.5 p-1.5">
         <span className="sr-only">Your Company</span>
         {/* <img className="h-8 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt=""/> */}
-        <img src="https://dsahoo.netlify.app/Assets/profile/profile-2.jpg" 
+        <img src="https://github.com/user-attachments/assets/1d7cf27c-ca0a-4178-8c55-5f1cbb812a58" 
                alt="Front of men&#039;s Basic Tee in black."
                className=" object-cover object-center h-9 w-9"
                />
