f3dc52bf93d7283390e8d0d34a2113ca5662fd02
Dhananjay Sahoo <kanha94377@gmail.com>
2024-08-18 05:51:07 +0530

    update

--- src/components/Header.js
+++ src/components/Header.js
@@ -126,7 +126,7 @@ export default function Header() {
 
       <a href="#" className="text-sm font-semibold leading-6 text-gray-900">Features</a>
       <a href="#" className="text-sm font-semibold leading-6 text-gray-900">Marketplace</a>
-      <a href="#" className="text-sm font-semibold leading-6 text-gray-900">Company</a>
+      <a href="#" className="text-sm font-semibold leading-6 text-gray-900">Trending</a>
     </div>
     <div className="hidden lg:flex lg:flex-1 lg:justify-end">
       <a href="#" className="text-sm font-semibold leading-6 text-gray-900">Log in <span aria-hidden="true">&rarr;</span></a>
